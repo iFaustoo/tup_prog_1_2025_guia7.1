a15985e21caeaef7d684ad7958c6f554b437bfc3
iFaustoo <faustoordoriguez10@gmail.com>
2025-06-06 12:22:16 -0300

    Add files via upload

---
+++ e2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace budines
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double masakg, masag, sobrante;
+            double budines = 0, paquetes = 0, cajas = 0;
+            Console.Write("Ingrese cantidad de masa en KG: ");
+            masakg = Convert.ToDouble(Console.ReadLine());
+            masag = masakg * 1000;
+            budines = Convert.ToInt32(masag / 55);
+            sobrante = masag % 55;
+            paquetes = Convert.ToInt32(budines / 12);
+            cajas = Convert.ToInt32(paquetes / 20);
+            Console.WriteLine("La cantidad de masa sobrante es de {0:f2}g", sobrante);
+            Console.WriteLine("Se producieron en total " + budines +  " budines, armando " + paquetes + " paquetes y finalmente " + cajas + " cajas.");
+
+        }
+    }
+}
